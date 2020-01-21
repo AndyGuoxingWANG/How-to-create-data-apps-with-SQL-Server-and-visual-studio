3af58eb529566b5891e179085462b4b609b4beca
AndyGuoxingWANG <andy.guoxingwang@gmail.com>
2020-01-21 16:24:13 -0500

    Add files via upload

---
+++ LoginForm/LoginForm/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace LoginForm
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.UserNameTextBox = new System.Windows.Forms.TextBox();
+            this.LogIn_Button = new System.Windows.Forms.Button();
+            this.LogOut_Button = new System.Windows.Forms.Button();
+            this.UserNameLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(289, 208);
+            this.PasswordTextBox.Multiline = true;
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.Size = new System.Drawing.Size(441, 57);
+            this.PasswordTextBox.TabIndex = 0;
+            // 
+            // UserNameTextBox
+            // 
+            this.UserNameTextBox.Location = new System.Drawing.Point(289, 57);
+            this.UserNameTextBox.Multiline = true;
+            this.UserNameTextBox.Name = "UserNameTextBox";
+            this.UserNameTextBox.Size = new System.Drawing.Size(441, 57);
+            this.UserNameTextBox.TabIndex = 1;
+            this.UserNameTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // LogIn_Button
+            // 
+            this.LogIn_Button.BackColor = System.Drawing.Color.Maroon;
+            this.LogIn_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.LogIn_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogIn_Button.ForeColor = System.Drawing.Color.Gold;
+            this.LogIn_Button.Location = new System.Drawing.Point(553, 338);
+            this.LogIn_Button.Name = "LogIn_Button";
+            this.LogIn_Button.Size = new System.Drawing.Size(177, 53);
+            this.LogIn_Button.TabIndex = 2;
+            this.LogIn_Button.Text = "LOGIN";
+            this.LogIn_Button.UseVisualStyleBackColor = false;
+            this.LogIn_Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LogOut_Button
+            // 
+            this.LogOut_Button.BackColor = System.Drawing.Color.Maroon;
+            this.LogOut_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.LogOut_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogOut_Button.ForeColor = System.Drawing.Color.Gold;
+            this.LogOut_Button.Location = new System.Drawing.Point(289, 338);
+            this.LogOut_Button.Name = "LogOut_Button";
+            this.LogOut_Button.Size = new System.Drawing.Size(178, 53);
+            this.LogOut_Button.TabIndex = 3;
+            this.LogOut_Button.Text = "LOGOUT";
+            this.LogOut_Button.UseVisualStyleBackColor = false;
+            this.LogOut_Button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // UserNameLabel
+            // 
+            this.UserNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserNameLabel.Location = new System.Drawing.Point(10, 57);
+            this.UserNameLabel.Name = "UserNameLabel";
+            this.UserNameLabel.Size = new System.Drawing.Size(239, 57);
+            this.UserNameLabel.TabIndex = 4;
+            this.UserNameLabel.Text = "USERNAME";
+            this.UserNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordLabel.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.PasswordLabel.Location = new System.Drawing.Point(3, 208);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(246, 57);
+            this.PasswordLabel.TabIndex = 5;
+            this.PasswordLabel.Text = "PASSWORD";
+            this.PasswordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.PasswordLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UserNameLabel);
+            this.Controls.Add(this.LogOut_Button);
+            this.Controls.Add(this.LogIn_Button);
+            this.Controls.Add(this.UserNameTextBox);
+            this.Controls.Add(this.PasswordTextBox);
+            this.ForeColor = System.Drawing.Color.SpringGreen;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LOGIN FORM";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.TextBox UserNameTextBox;
+        private System.Windows.Forms.Button LogIn_Button;
+        private System.Windows.Forms.Button LogOut_Button;
+        private System.Windows.Forms.Label UserNameLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+    }
+}
+
---
+++ LoginForm/LoginForm/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace LoginForm
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=DVRLT4VKDYF2;Initial Catalog=UdemyUserLogin;Integrated Security=True");
+            string query = "select * from logins where username ='"+UserNameTextBox.Text.Trim()+"'and password ='"+PasswordTextBox.Text.Trim()+"'";
+
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dtb1 = new DataTable();
+            sda.Fill(dtb1);
+            if (dtb1.Rows.Count == 1)
+            {
+                frmLoggedIn objfrmLoggedIn = new frmLoggedIn();
+                this.Hide();
+                objfrmLoggedIn.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please check your username and password");
+            }
+        }
+    }
+}
